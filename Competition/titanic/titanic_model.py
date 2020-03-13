5630f50729c6f2d9d527b0d564a774fabb2140c4
jwant <62126895+jwant@users.noreply.github.com>
2020-03-13 16:20:45 +1100

    Inital Predictions
    ADD: first set of predictions using tree method

---
+++ Competition/titanic/titanic_model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+
+features = ["Pclass","Sex","SibSp","Parch"]
+x_train = pd.get_dummies(train_data[features])
+x_test = pd.get_dummies(test_data[features])
+
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(x_train,train_data['Survived'])
+predictions = model.predict(x_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('predictions.csv')
